ceaf1fdf2f5e5c7ae12bd980a12ef637cc43e086
Anthony Atkinson <ep1cantw0n@gmail.com>
2023-01-28 15:04:32 -0600

    first commit

---
+++ alfven_estimates.py
@@ -0,0 +1,293 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+calculate = False
+
+AVK = 0.64
+dAVK = 0.12
+BVK = 0.25
+dBVK = 0.08
+
+AM = 2.33
+dAM = 0.06
+BM = -1.50
+dBM = 0.21
+CM = 0.31
+dCM = 0.17
+
+LN10 = np.log(10)
+
+# wright et al 2018 (eqn 5): valid for range 1.1 < V-K < 7.0
+def taucVK(V, K):
+    if np.fabs(V - K - 4.05) <= 2.95:
+        return pow(10., AVK + BVK * (V - K))
+    else:
+        return np.nan
+    
+def dtaucVK(V, K, dV, dK):
+    return taucVK(V, K) * LN10 * np.sqrt(pow(dAVK, 2.) + 
+                                         pow((V - K) * dBVK, 2.) + 
+                                         pow(BVK * np.sqrt(pow(dV, 2.) + pow(dK, 2.)), 2.))
+
+def RoVK(Prot, V, K):
+    return np.fabs(Prot) / taucVK(V,K)
+
+def dRoVK(Prot, V, K, dProt, dV, dK):
+    return RoVK(Prot, V, K) * np.sqrt(pow(dProt / Prot, 2.) +
+                                      pow(dtaucVK(V, K, dV, dK) / taucVK(V, K), 2.))
+
+# wright et al 2018 (eqn 6): valid for range 0.08 < M/Msol < 1.36
+#M in solar masses
+def taucM(M):
+    if np.fabs(M - 0.72) <= 0.64:
+        return pow(10., AM + BM * M + CM * pow(M, 2.)) 
+    else:
+        return np.nan
+    
+def dtaucM(M, dM):
+    return taucM(M) * LN10 * np.sqrt(pow(dAM, 2.) +
+                                     pow(M * dBM, 2.) +
+                                     pow(pow(M, 2.) * dCM, 2.) +
+                                     pow((2 * M * CM + BM) * dM, 2.))
+    
+def RoM(Prot, M):
+    return np.fabs(Prot) / taucM(M)
+
+def dRoM(Prot, M, dProt, dM):
+    return RoM(Prot, M) * np.sqrt(pow(dProt / Prot, 2.) + 
+                                  pow(dtaucM(M, dM) / taucM(M), 2.))
+
+def RoAvg(RoVK, RoM):
+    return np.nanmean([RoVK, RoM])
+    
+def dRoAvg(dRoVK, dRoM):
+    return np.sqrt(np.nansum([pow(dRoVK, 2.),pow(dRoM, 2.)]))
+
+x = np.linspace(0.,2.7,1000) # ro/ro sol x vals
+prot_sol = 27.
+ro_sol = 1.85
+dro_sol = 0.26
+ra_sol = 0.1
+dra_sol = 0.02
+r_sol = 1.
+s = -1.38
+ds = 0.14
+r = -0.16
+dr = 0.13
+# all values scaled to solar values at maximum
+# using Brun-Browning p23
+# combining eqns: (7) Farrish 2019 and (2) Farrish 2021
+def ra_schrijver(ro, rad):
+    return ra_sol * np.real(pow(ro / ro_sol, s * r) * pow(rad / r_sol, 2 * r))
+
+def dra(ro, rad, dro, drad):
+    return ra_schrijver(ro, rad) * np.sqrt(pow(dra_sol / ra_sol, 2.) +
+                                           pow(s * r *  np.sqrt(pow(dro / ro, 2.) + pow(dro_sol / ro_sol, 2.)), 2.) +
+                                           pow(2 * r * drad / rad, 2.) +
+                                           pow(r * np.log(ro / ro_sol) * ds, 2.) +
+                                           pow((s * np.log(ro / ro_sol) + 2 * s * np.log(rad)) * dr, 2.))
+
+def measured_uncertainties(nasa_exo):
+    nasa_exo['pl_bmasseerr'] = nasa_exo.apply(lambda x: np.max([x['pl_bmasseerr1'],np.fabs(x['pl_bmasseerr2'])]), axis=1)
+    nasa_exo['pl_orbsmaxerr'] = nasa_exo.apply(lambda x: np.max([x['pl_orbsmaxerr1'],np.fabs(x['pl_orbsmaxerr2'])]), axis=1)
+    nasa_exo['pl_orbeccenerr'] = nasa_exo.apply(lambda x: np.max([x['pl_orbeccenerr1'],np.fabs(x['pl_orbeccenerr2'])]), axis=1)
+    nasa_exo['pl_radeerr'] = nasa_exo.apply(lambda x: np.max([x['pl_radeerr1'],np.fabs(x['pl_radeerr2'])]), axis=1)
+    nasa_exo['st_masserr'] = nasa_exo.apply(lambda x: np.max([x['st_masserr1'],np.fabs(x['st_masserr2'])]), axis=1)
+    nasa_exo['st_raderr'] = nasa_exo.apply(lambda x: np.max([x['st_raderr1'],np.fabs(x['st_raderr2'])]), axis=1)
+    nasa_exo['sy_disterr'] = nasa_exo.apply(lambda x: np.max([x['sy_disterr1'],np.fabs(x['sy_disterr2'])]), axis=1)
+    nasa_exo['sy_kmagerr'] = nasa_exo.apply(lambda x: np.max([x['sy_kmagerr1'],np.fabs(x['sy_kmagerr2'])]), axis=1)
+    nasa_exo['sy_vmagerr'] = nasa_exo.apply(lambda x: np.max([x['sy_vmagerr1'],np.fabs(x['sy_vmagerr2'])]), axis=1)
+    return nasa_exo
+
+def estimate_rossby(prot_data):
+    prot_data['RoVK'] = prot_data.apply(lambda x: RoVK(x['Prot'], x['sy_vmag'], x['sy_kmag']), axis=1)
+    prot_data['dRoVK'] = prot_data.apply(lambda x: dRoVK(x['Prot'], x['sy_vmag'], x['sy_kmag'], x['e_Prot'], x['sy_vmagerr'], x['sy_kmagerr']), axis=1)
+    prot_data['TaucVK'] = prot_data.apply(lambda x: taucVK(x['sy_vmag'], x['sy_kmag']), axis=1)
+    prot_data['dTaucVK'] = prot_data.apply(lambda x: dtaucVK(x['sy_vmag'], x['sy_kmag'], x['sy_vmagerr'], x['sy_kmagerr']), axis=1)
+    prot_data['RoM'] = prot_data.apply(lambda x: RoM(x['Prot'], x['st_mass']), axis=1)
+    prot_data['dRoM'] = prot_data.apply(lambda x: dRoM(x['Prot'], x['st_mass'], x['e_Prot'], x['st_masserr']), axis=1)
+    prot_data['TaucM'] = prot_data.apply(lambda x: taucM(x['st_mass']), axis=1)
+    prot_data['dTaucM'] = prot_data.apply(lambda x: dtaucM(x['st_mass'], x['st_masserr']), axis=1)
+    prot_data['RoAvg'] = prot_data.apply(lambda x: RoAvg(x['RoVK'], x['RoM']), axis=1)
+    prot_data['dRoAvg'] = prot_data.apply(lambda x: dRoAvg(x['dRoVK'], x['dRoM']), axis=1)
+
+    #choose which Ro is used in plots
+    prot_data['Ro'] = prot_data['RoM']
+    prot_data['dRo'] = prot_data['dRoM']
+
+    return prot_data
+
+def estimate_alfven(data):
+    
+    # Estimate value and uncertainty for periastron
+    data['rperi'] = data.apply(lambda x: x['pl_orbsmax'] * (1 - x['pl_orbeccen']), axis=1)
+    data['drperi'] = data.apply(lambda x: np.sqrt(np.nansum([
+    pow(x['pl_orbsmaxerr'] * (1 - x['pl_orbeccen']),2.),
+    pow(x['pl_orbsmax'] * x['pl_orbeccenerr'], 2.)])), axis=1)
+
+    # Calculate Shrijver scaling relation for mean AS radius
+
+    # Estimate closest distance from star vs mean AS radius ratio
+    alfven = data.apply(lambda x: x['rperi'] / ra_schrijver(x['Ro'], x['st_rad']), axis=1)
+    dalfven = data.apply(lambda x: x['rperi'] / ra_schrijver(x['Ro'], x['st_rad']) * np.sqrt(pow(x['drperi'] / x['rperi'], 2.) +
+                        pow(dra(x['Ro'], x['st_rad'], x['dRo'], x['st_raderr'] / ra_schrijver(x['Ro'], x['st_rad'])), 2.)), axis=1)
+    alfven_data = data.copy(deep=False)
+    alfven_data['orbit2alfven'] = alfven
+    alfven_data['dorbit2alfven'] = dalfven
+    
+    # select out data where the Stellar Radius entry is non-empty
+    alfven_data = alfven_data[alfven_data['st_rad'].notnull()]
+    
+    #optional: remove 99th quantile and bad vals
+    alfven_data = alfven_data[(alfven_data['Ro'] < np.quantile(alfven_data['Ro'], 0.99)) & (alfven_data['orbit2alfven'] < np.quantile(alfven_data['orbit2alfven'], 0.99))]
+    alfven_data = alfven_data[(alfven_data['Ro'] > 0.0) & (alfven_data['orbit2alfven'] > 0.0)]
+
+    return alfven_data
+
+CLASS_COLORS = ['#DDBB44','#00AA00','#FF5500','#00FFFF']
+CLASS_LABELS = ['subterran', 'terran', 'superterran', 'giant']
+
+def mass_class(pl_mass):
+    # subterran - 0.1 to 0.5 ME 
+    if np.fabs(pl_mass - 0.3) <= 0.2:
+        return 0
+    # terran - 0.5 to 3.0 ME
+    elif np.fabs(pl_mass - 1.75) <= 1.25:
+        return 1
+    # superterran - 3.0 to 10. ME
+    elif np.fabs(pl_mass - 6.5) <= 3.5:
+        return 2
+    # giant - > 10. ME
+    elif pl_mass > 10.:
+        return 3
+    else:
+        return -1
+
+def rad_class(pl_rad):
+    # subterran - 0.4 to 0.8 RE
+    if np.fabs(pl_rad - 0.6) <= 0.2:
+        return 0
+    # terran - 0.8 to 1.6 RE
+    elif np.fabs(pl_rad - 1.2) <= 0.4:
+        return 1
+    # superterran - 1.6 to 2.5 RE
+    elif np.fabs(pl_rad - 2.05) <= 0.45:
+        return 2
+    # giant - > 2.5 RE
+    elif pl_rad > 2.5:
+        return 3
+    else:
+        return -1
+
+
+def planet_classes(alfven_data):
+
+    exos_habitable = pd.read_csv('current-exo-data/exos_habitable.csv')
+    
+    alfven_data['mass_class'] = alfven_data.apply(lambda r: mass_class(r.pl_bmasse), axis=1)
+    alfven_data['rad_class'] = alfven_data.apply(lambda r: rad_class(r.pl_rade), axis=1)
+    alfven_data['habitable'] = alfven_data.apply(lambda r: 1 if r.pl_name in set(exos_habitable.pl_name) else 0, axis=1)
+
+    return alfven_data
+
+def calculate_exos():
+    nasa_exo = pd.read_csv('current-exo-data/nasa_exo.csv')
+    nasa_exo = measured_uncertainties(nasa_exo)
+    exos = pd.read_csv('current-exo-data/exos.csv')
+
+    # List exoplanets with a host star that has a rotation period + stats
+    prot_col_list = ['pl_name','hostname','Prot','e_Prot','pl_bmasse',
+        'pl_bmasseerr','pl_bmassprov','pl_rade','pl_radeerr','pl_orbsmax',
+        'pl_orbsmaxerr','pl_orbeccen','pl_orbeccenerr','sy_vmag','sy_vmagerr',
+        'sy_kmag','sy_kmagerr','st_rad','st_raderr','st_mass','st_masserr',
+        'sy_dist','sy_disterr','KOI','KIC','TIC','GAIA','db']
+    prot_data = pd.merge(nasa_exo, exos, how='inner', on='hostname', suffixes=('_x', None))[prot_col_list]
+    
+    # Estimate Ro from Wright et. al. 2018 Tc formulae
+    prot_data = estimate_rossby(prot_data)
+
+    # List exoplanets that have all of the relevant stats: Ro, a, e 
+    data_col_list = ['pl_name','pl_orbsmax','pl_orbsmaxerr','pl_orbeccen',
+        'pl_orbeccenerr','pl_rade','pl_bmasse','pl_bmassprov','st_rad',
+        'st_raderr','RoVK','RoM','RoAvg','Ro','dRo','KOI','KIC','TIC','GAIA','db']
+    
+    data = prot_data[prot_data['Ro'].notnull() & prot_data['pl_orbsmax'].notnull() & prot_data['pl_orbeccen'].notnull()]
+    data = data[data_col_list].copy()
+
+    # List exoplanets with periastron to mean AS ratio
+    alfven_data = estimate_alfven(data)
+
+    # Assign size and habitability classifications to each planet
+    alfven_data = planet_classes(alfven_data)
+
+    alfven_data.to_csv('current-exo-data/alfven_data.csv',index=False)
+
+def plot(alfven_data, condition, title, imgname, errorbars=False, names=False):
+    # should figure out way to make more interactive (choose class, subset, etc)
+    # must speed up data input
+    # maybe add data series on top of each other?
+    subset = alfven_data[condition]
+    xlim = np.ceil(2 * np.max(subset.Ro)) / 2.
+    ylim = np.ceil(2 * np.max(subset.orbit2alfven)) / 2.
+
+    labelled = [subset[subset.mass_class == i].iat[0,0] for i in range(0,4)]
+
+    subset_rows = subset.iterrows()
+
+    fig = plt.figure(figsize=(15,8))
+    ax = fig.add_subplot(111)
+    # ax.errorbar(habitable_alfven_all['Ro'], habitable_alfven_all['orbit2alfven'], xerr=habitable_alfven_all['dRo'], yerr=habitable_alfven_all['dorbit2alfven'], linestyle='None', ecolor='#333333')
+
+    for idx, row in subset_rows:
+        xpt = row.Ro
+        ypt = row.orbit2alfven
+        ax.scatter(xpt, ypt, color=CLASS_COLORS[row.mass_class], s=75, label=(CLASS_LABELS[row.mass_class] if row.pl_name in labelled else ''))
+        if errorbars:
+            ax.errorbar(xpt, ypt, xerr=row.dRo, yerr=row.dorbit2alfven, linestyle='None', ecolor='#333333')
+        
+        if names:
+            lbl = row.pl_name
+            ax.text(xpt + 0.02, ypt + 0.01, lbl, fontsize=20)
+        
+    ax.set_title(title,fontsize=25)
+    ax.set_xlabel("Ro",fontsize=20)
+    ax.set_ylabel("rp/RA",fontsize=20)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    ax.grid(visible=True)
+    ax.set_xlim([0.0,xlim])
+    ax.set_ylim([0.0,ylim])
+    ax.legend(loc=2,fontsize=15)
+    fig.set_facecolor('white')
+    plt.axhline(y = 1.0, xmin = 0.00, xmax = xlim, linestyle='--', color='#666666')
+    plt.text(xlim + .02, 0.98, 'AS', fontsize=15)
+
+    plt.savefig(imgname, format='png')
+
+'''
+ra_data = data.apply(lambda x: ra_schrijver(x['Ro'], x['st_rad']), axis=1)
+ro_data = data['Ro']
+rad_data = data['st_rad']
+
+#the alison plot
+plt.scatter(ro_data / ro_sol, ra_data / ra_sol)
+plt.plot(x, ra_schrijver(x, 1.) / ra_sol, c='black')
+plt.xlim((0.0,ro_sol))
+'''
+
+def main():
+
+    if calculate:
+        alfven_data = calculate_exos()
+    else:
+        alfven_data = pd.read_csv('current-exo-data/alfven_data.csv')
+
+    condition = alfven_data.habitable == 1
+    title = "Habitable Exoplanet Periastrons and RA Estimates"
+    imgname = 'current-exo-data/plot_habitable.png'
+    plot(alfven_data, condition, title, imgname, errorbars=True, names=True)
+
+if __name__ == '__main__':
+    main()
---
+++ update_exos_db.py
@@ -0,0 +1,226 @@
+import numpy as np
+import pandas as pd
+import re
+from os import system
+
+# FETCH NEW EXO DATA:
+# > cat nasa_exo_query.txt | xargs wget -o nasa_exo_PSCP.csv
+fetch_DBs = False
+update_exos = True
+load_exos = True
+
+'''
+DATA/COLUMNS THAT ARE NEEDED
+ - Prot/PRot/st_rotp
+ - pl_name (Planet Name)
+   - kic_id/kepid
+   - koi/koi_name
+   - tic_id
+   - toi
+   - gaia_id
+ - pl_bmasse (Planet Mass in Earth Masses)
+ - pl_rade (Planet Radius in Earth Radii)
+ - pl_orbsmax (Planet Orbit Semi-Major Axis in AU)
+ - pl_orbeccen (Planet Orbit Eccentricity)
+
+ - sy_vmag (V-band magnitude of star)
+ - sy_kmag (K-band magnitude of star)
+ - st_rad (Stellar Radius in Solar Radii)
+ - st_mass (Stellar Mass in Solar Masses)
+
+ - notes:
+   - ALL parameters EXCEPT names/ids and Prots are available via nasa/caltech
+     - currently selecting names+prots from surveys and merging onto nasa
+   - may want to use consistent parameter set (dont use PSCP db, instead use
+   PS database and select only where 'Default Parameter Set is 1') or parameter
+   set offered by the paper offering the Prot
+'''
+
+def init_nasa_exo():
+
+    # Load NASA exoplanet database
+    nasa_exo = pd.read_csv("nasa_exo_PSCP.csv")
+
+    # !! need to fetch new caltech DB of Kep confs and update
+    # Add KOI/KIC ID columns of confirmed Kepler planets
+    p_koi = re.compile(r'^K(\d+)\.\d{2}$')
+    nasa_kep = pd.read_csv("nasa_exo_kep.csv")
+    nasa_kep['KOI'] = nasa_kep.apply(lambda r: int(r.koi_name[1:-3]) if p_koi.match(str(r.koi_name)) is not None else np.nan, axis=1)
+    nasa_kep.rename(columns={'kepid': 'KIC'}, inplace=True)
+    nasa_exo = pd.merge(nasa_exo, nasa_kep, how='left', on='pl_name')
+    nasa_exo['KOI'].fillna(-1, inplace=True, downcast='infer')
+    nasa_exo['KIC'].fillna(-1, inplace=True, downcast='infer')
+
+    # Add TIC ID column
+    p_tic = re.compile(r'^TIC (\d+)$')
+    nasa_exo['TIC'] = nasa_exo.apply(lambda r: int(str(r.tic_id)[4:]) if p_tic.match(str(r.tic_id)) is not None else -1, axis=1)
+
+    # Add GAIA ID columns
+    nasa_exo['GAIA'] = nasa_exo.apply(lambda x: int(x['gaia_id'][9:]) if re.match(r'^Gaia DR2 \d+$', str(x['gaia_id'])) is not None else np.nan, axis=1)
+
+    nasa_exo.to_csv('current-exo-data/nasa_exo.csv', index=False)
+
+    return nasa_exo
+
+# Update the short-list of exoplanet systems/stars shared by mcq13 and nasa
+def update_exos_mcq13(nasa_exo):
+
+    # Load McQuillan 2013 KOI stellar rotation rate survey
+    mcq13_kois = pd.read_csv("prot_mcq_2013.dat", sep="\s+")
+    mcq13_kois = mcq13_kois[['KOI','PRot', 'e_PRot']]
+    mcq13_kois.rename(columns={'PRot':'Prot','e_PRot': 'e_Prot'}, inplace=True)
+    mcq13_kois.insert(len(mcq13_kois.columns), 'db', 'mcq13')
+    mcq13_kois = mcq13_kois[mcq13_kois.Prot.notnull()]
+
+    # Select stars from mcq13 whose KOI is in nasa db
+    exos_mcq13 = pd.merge(nasa_exo, mcq13_kois, how='inner', on='KOI')[['hostname','KOI','Prot','e_Prot', 'db']]
+    exos_mcq13.drop_duplicates(subset='hostname', inplace=True)
+    exos_mcq13.to_csv('current-exo-data/exos_mcq13.csv', index=False)
+    return exos_mcq13
+    
+def update_exos_mcq14(nasa_exo):
+
+    # Load McQuillan 2014 KIC stellar rotation rate survey
+    mcq14_kics = pd.read_csv("prot_mcq_2014.dat", sep="\s+")
+    mcq14_kics = mcq14_kics[['KIC','PRot', 'e_PRot']]
+    mcq14_kics.rename(columns={'PRot':'Prot','e_PRot': 'e_Prot'}, inplace=True)
+    mcq14_kics.insert(len(mcq14_kics.columns), 'db', 'mcq14')
+    mcq14_kics = mcq14_kics[mcq14_kics.Prot.notnull()]
+
+    exos_mcq14 = pd.merge(nasa_exo, mcq14_kics, how='inner', on='KIC')[['hostname','KIC','Prot','e_Prot', 'db']]
+    exos_mcq14.drop_duplicates(subset='hostname', inplace=True)
+    exos_mcq14.to_csv('current-exo-data/exos_mcq14.csv', index=False)
+    return exos_mcq14
+
+def update_exos_mar20(nasa_exo):
+
+    # Load Martin 2020 TOI stellar rotation period survey
+    mar20 = pd.read_csv("prot_martin_2020_tic.csv", usecols=[0,1,2,3,4])
+    mar20['TIC'] = mar20.apply(lambda x: int(x['TIC_ID']), axis=1)
+    mar20.drop(columns='TIC_ID',inplace=True)
+    mar20.rename(columns={'eProt':'e_Prot'}, inplace=True)
+    mar20.insert(len(mar20.columns), 'db', 'mar20')
+    mar20 = mar20[mar20.Prot.notnull()]
+
+    # Selection criteria for rotation rates based on their classification/certainty
+    #selection = (mar20['Variability_Classification'] == "Unambiguous_Rotation") & mar20['Prot'].notnull()
+    selection = ((mar20['Variability_Classification'] == "Unambiguous_Rotation") | (mar20['Variability_Classification'] == "Dubious_Rotation")) & mar20['Prot'].notnull()
+    mar20_prot = mar20[selection].copy(deep=False)
+    mar20_prot['Prot'] = mar20_prot.apply(lambda x: select_prot_martin(x['Prot']), axis=1)
+
+    exos_mar20 = pd.merge(nasa_exo, mar20_prot, how='inner', on='TIC')[['hostname','TIC','Prot','e_Prot', 'db']]
+    exos_mar20.drop_duplicates(subset='hostname', inplace=True)
+    exos_mar20.to_csv('current-exo-data/exos_mar20.csv', index=False)
+    return exos_mar20
+
+# fn can be np.mean/min/max, lambda x: np.nan
+def select_prot_martin(prot_str, fn=np.min):
+    if "/" in str(prot_str):
+        prots = prot_str.split("/")
+        protf = []
+        for s in prots:
+            protf.append(float(s))
+        return fn(protf)
+    else: return float(prot_str)
+
+def update_exos_custom(nasa_exo):
+    arm16_prot = pd.read_csv("custom_prot.txt", sep="\s+", header=14, nrows=7)
+    
+    arm16_prot = arm16_prot[['hostname','prot_acf','eprot_acf']]
+    arm16_prot.rename(columns={'prot_acf':'Prot','eprot_acf':'e_Prot'}, inplace=True)
+    arm16_prot.insert(len(arm16_prot.columns), 'db', 'arm16')
+    arm16_prot = arm16_prot[arm16_prot.Prot.notnull()]
+
+    exos_arm16 = pd.merge(nasa_exo, arm16_prot, how='inner', on='hostname')[['hostname','Prot','e_Prot', 'db']]
+    exos_arm16.drop_duplicates(subset='hostname', inplace=True)
+    exos_arm16.to_csv('current-exo-data/exos_arm16.csv', index=False)
+    return exos_arm16
+
+def update_exos_lu22(nasa_exo):
+    lu22_gaia = pd.read_csv('lu_gaia_prot.txt', header=21, sep="\s+")
+
+    lu22_gaia = lu22_gaia[['GAIA','Prot']]
+    lu22_gaia.insert(len(lu22_gaia.columns), 'db', 'lu22')
+    lu22_gaia = lu22_gaia[lu22_gaia.Prot.notnull()]
+
+    # problem here: discrepancy in number of entries
+    # exos_lu22 = lu22_gaia[lu22_gaia.GAIA.isin(nasa_exo.GAIA)]
+
+    exos_lu22 = pd.merge(nasa_exo, lu22_gaia, how='inner', on='GAIA')[['hostname','GAIA','Prot', 'db']]
+    exos_lu22.drop_duplicates(subset='hostname', inplace=True)
+
+    exos_lu22.to_csv('current-exo-data/exos_lu22.csv',index=False)
+    return exos_lu22
+
+def update_exos_nasa(nasa_exo):
+    exos_nasa = nasa_exo[nasa_exo.st_rotp.notnull()][['hostname','st_rotp','st_rotperr1','st_rotperr2']]
+    exos_nasa['st_rotperr'] = nasa_exo.apply(lambda r: np.max([r.st_rotperr1, np.fabs(r.st_rotperr2)]), axis=1)
+    exos_nasa.rename(columns={'st_rotp':'Prot', 'st_rotperr': 'e_Prot'}, inplace=True)
+    exos_nasa.drop_duplicates(subset='hostname', inplace=True)
+    exos_nasa.drop(columns=['st_rotperr1','st_rotperr2'], inplace=True)
+    exos_nasa.insert(len(exos_nasa.columns), 'db', 'nasa')
+
+    exos_nasa.to_csv('current-exo-data/exos_nasa.csv', index=False)
+
+    return exos_nasa
+
+def update_exos_habitable(nasa_exo):
+    habitable = pd.read_csv('habitable.txt', header=1)
+
+    exos_habitable = pd.merge(nasa_exo, habitable, how='inner', on='pl_name')[['hostname','pl_name']]
+
+    exos_habitable.to_csv('current-exo-data/exos_habitable.csv', index=False)
+
+def main():
+    if fetch_DBs:
+
+        print('Updating Planetary Systems Composite Parameters (pscomppars) database!')
+        # Get updated Planetary Systems Combined Parameters DB
+        system('cat nasa_exo_query.txt | xargs wget -o log_PSCP -O nasa_exo_PSCP.csv')
+
+        print('Updating Kepler confirmed planets (kepnames) database!')
+        system('cat nasa_exo_kep_query.txt | xargs wget -o log_kep -O nasa_exo_kep.csv')
+
+    if update_exos:
+
+        nasa_exo = init_nasa_exo()
+
+        exos_mcq13 = update_exos_mcq13(nasa_exo)
+        exos_mcq14 = update_exos_mcq14(nasa_exo)
+        exos_arm16 = update_exos_custom(nasa_exo)
+        exos_mar20 = update_exos_mar20(nasa_exo)
+        exos_lu22 = update_exos_lu22(nasa_exo)
+        exos_nasa = update_exos_nasa(nasa_exo)
+        update_exos_habitable(nasa_exo)
+
+        print(exos_mcq13.count())
+        print(exos_mcq14.count())
+        print(exos_arm16.count())
+        print(exos_mar20.count())
+        print(exos_lu22.count())
+        print(exos_nasa.count())
+
+    if load_exos:
+        exos_mcq13 = pd.read_csv('current-exo-data/exos_mcq13.csv')
+        exos_mcq14 = pd.read_csv('current-exo-data/exos_mcq14.csv')
+        exos_arm16 = pd.read_csv('current-exo-data/exos_arm16.csv')
+        exos_mar20 = pd.read_csv('current-exo-data/exos_mar20.csv')
+        exos_lu22 = pd.read_csv('current-exo-data/exos_lu22.csv')
+        exos_nasa = pd.read_csv('current-exo-data/exos_nasa.csv')
+
+        exos = pd.concat([exos_mcq13, exos_mcq14, exos_arm16, exos_mar20, exos_lu22, exos_nasa], ignore_index=True)
+        exos['e_Prot'] = exos.apply(lambda r: np.nan if r.e_Prot == 0.0 else r.e_Prot, axis=1)
+        exos['KOI'].fillna(-1, inplace=True, downcast='infer')
+        exos['KIC'].fillna(-1, inplace=True, downcast='infer')
+        exos['TIC'].fillna(-1, inplace=True, downcast='infer')
+        exos['GAIA'].fillna(-1, inplace=True, downcast='infer')
+
+        exos.sort_values('e_Prot', inplace=True, ignore_index=True, ascending=True)
+        exos.drop_duplicates(subset='hostname', inplace=True)
+        exos = exos[['hostname', 'Prot', 'e_Prot', 'KOI', 'KIC', 'TIC', 'GAIA', 'db']]
+        exos.to_csv('current-exo-data/exos.csv', index=False)
+        print(exos.count())
+    
+
+if __name__ == '__main__':
+    main()

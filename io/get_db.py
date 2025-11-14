0c570bf1493c634fc1c2fc0adc96962ebdb0b463
Anthony Atkinson <asa15@rice.edu>
2025-11-13 18:00:59 -0600

    prot master table

--- io/get_db.py
+++ io/get_db.py
@@ -1,13 +1,17 @@
+from datetime import datetime
+import numpy as np
 import os
 import pandas as pd
 import pyvo as vo
 import re
 
-from aliases import pool_aliases
-
 # create object that will service TAP queries to NEA TAP server
 url_NEA = "https://exoplanetarchive.ipac.caltech.edu/TAP"
-service = vo.dal.TAPService(url_NEA)
+service_NEA = vo.dal.TAPService(url_NEA)
+
+# create object that will service TAP queries to SIMBAD TAP server
+url_SIMBAD = "https://simbad.unistra.fr/simbad/sim-tap"
+service_SIMBAD = vo.dal.TAPService(url_SIMBAD)
 
 #------------------------------------------------------------------------------
 # COLUMNS TO FETCH
@@ -66,123 +70,106 @@ toi_cols = [
 
 
 
-def fetch_table(table_name: str, cols: list[str], cond_str: str=None):
-    """
-    Fetch the data for a named table in the NEA table list using the Table
-    Access Protocol (TAP).
-    """
-
-    # combine list of table columns as one string separated by commas
-    cols_str = ",".join(cols)
-
-    # ADQL query
-    query = f"""
-        SELECT {cols_str}
-        FROM {table_name}
-        """
-
-    # send TAP request to VO server
-    resp = service.search(query)
-
-    # failed query ends process
-    print(f"Successful query for table `{table_name}`")
-
-    # extract data from response as pd.DataFrame object
-    table = resp.to_table()
-    table = table.to_pandas()
-    return table
-
-
-
-def update_aliases(hostnames: pd.Series, tag_list: list[str], fname: str):
-    """
-    Update aliases table for the specified hostnames and tags/catalogs
-    """
-
-    if os.path.isfile(fname_al):
-        al = pd.read_csv(fname_al)
-        al_tags = set(al.columns)
-        al_hosts = set(al["hostname"])
-        
-        tags_set = set(tag_list)
-        hosts_set = set(hostnames)
-
-        tags_diff = set(tag_list) - al_tags
-        hosts_diff = set(hostnames) - al_hosts
-
-        # same tags but new hosts -- append new rows
-        if len(tags_diff) == 0 and len(hosts_diff) > 0:
-            print("Tags are the same but the hosts differ -- adding new rows")
-            hostnames = pd.Series(list(hosts_diff))
-            al_new = pool_aliases(hostnames, tag_list)
-            al = pd.concat([al, al_new], ignore_index=True)
-
-        # new tags but same hosts -- merge new columns
-        elif len(tags_diff) > 0 and len(hosts_diff) == 0:
-            print("Tags differ but the hosts are the same -- get new aliases")
-            tag_list = list(tags_diff)
-            al_new = pool_aliases(hostnames, tag_list)
-            al = pd.merge(al, al_new, on="hostname", how="inner")
-
-        # new tags and new hosts -- get all data
-        elif len(tags_diff) > 0 or len(hosts_diff) > 0:
-            print("Tags differ and there are new hosts -- getting new data")
-            tag_list = list(tags_diff)
-            hostnames = pd.Series(list(hosts_diff))
-            al = pool_aliases(hostnames, tag_list)
-        else:
-            print("File on disk is up to date!")
-
-    else:
-        # file doesn't exist on disk -- get all data
-        print("No aliases file exists -- getting new data")
-        al = pool_aliases(hostnames, tag_list)
-
-    return al
-
-
-
 if __name__ == "__main__":
 
-    ##### REQUEST TABLES
+    ##### NEA HOSTNAMES
 
-    # fetch tables from NEA using TAP
-    ps = fetch_table("pscomppars", ps_cols)
-    koi = fetch_table("cumulative", koi_cols)
-    k2 = fetch_table("k2pandc", k2_cols)
-    toi = fetch_table("toi", toi_cols)
-
-
-
-    ##### PSCP ALIASES
-
-    # load list of exoplanet host star names
-    hostnames = pd.Series(pd.unique(ps["hostname"]), name="hostname")
-
-    # fetch aliases from NEA using API endpoint or existing file on disk
-    fname_al = "../db/nea/aliases.csv"
-    tag_list = ["KOI", "KIC", "EPIC", "TOI", "TIC", "Gaia DR2", "Gaia DR3"]
-    al = update_aliases(hostnames, tag_list, fname_al)
-
-    # add aliases of confirmed exoplanets to PSCP table
-    ps = pd.merge(ps, al, how="left", on="hostname")
-
-    # stars lacking alias from a given catalog receive a -1 as integer placeholder value
-    ps[tag_list] = ps[tag_list].fillna(-1)
-    ps[tag_list] = ps[tag_list].astype(int)
+    url_NEA = "https://exoplanetarchive.ipac.caltech.edu/TAP"
+    service_NEA = vo.dal.TAPService(url_NEA)
+    query_NEA = """
+        SELECT pscomppars.hostname
+        FROM exo_tap.pscomppars
+        UNION
+        SELECT k2pandc.hostname
+        FROM exo_tap.k2pandc
+        UNION
+        SELECT 'KIC ' || CAST(cumulative.kepid AS VARCHAR(8)) AS hostname
+        FROM exo_tap.cumulative
+        UNION
+        SELECT 'TIC ' || CAST(toi.tid AS VARCHAR(11)) AS hostname
+        FROM exo_tap.toi
+    """
+    time = datetime.now()
+    res_NEA = service_NEA.run_sync(query_NEA, maxrec=100_000)
+    dt = datetime.now() - time
+    hostnames = res_NEA.to_table().to_pandas()
+    print(f"Exoplanet Host Names Identified ({int(dt.seconds/60)}m {dt.seconds%60}s)")
 
 
+    ##### SIMBAD ALIASES
+
+    url_SIMBAD = "https://simbad.unistra.fr/simbad/sim-tap"
+    service_SIMBAD = vo.dal.TAPService(url_SIMBAD)
+    query_SIMBAD = """
+        SELECT hostname, basic.main_id, id1.id AS kic, id2.id AS tic, id3.id AS epic, id4.id AS gaia_dr3_id, tbl.oidref
+        FROM (
+          SELECT hostnames.hostname, id1.oidref AS oidref
+          FROM TAP_UPLOAD.hostnames
+          LEFT JOIN ident as id1
+          ON id1.id = hostnames.hostname
+        ) AS tbl
+        LEFT JOIN (
+          SELECT ident.id, ident.oidref
+          FROM ident
+          WHERE ident.id LIKE 'KIC %'
+        ) AS id1
+        ON tbl.oidref = id1.oidref
+        LEFT JOIN (
+          SELECT ident.id, ident.oidref
+          FROM ident
+          WHERE ident.id LIKE 'TIC %'
+        ) AS id2
+        ON tbl.oidref = id2.oidref
+        LEFT JOIN (
+          SELECT ident.id, ident.oidref
+          FROM ident
+          WHERE ident.id LIKE 'EPIC %'
+        ) AS id3
+        ON tbl.oidref = id3.oidref
+        LEFT JOIN (
+          SELECT ident.id, ident.oidref
+          FROM ident
+          WHERE ident.id LIKE 'Gaia DR3 %'
+        ) AS id4
+        ON tbl.oidref = id4.oidref
+        LEFT JOIN basic
+        ON tbl.oidref = basic.oid;
+    """
+
+    time = datetime.now()
+    res_SIMBAD = service_SIMBAD.run_sync(query_SIMBAD, maxrec=100_000, uploads={"hostnames": res_NEA})
+    dt = datetime.now() - time
+    aliases = res_SIMBAD.to_table().to_pandas()
+    print(f"Aliases Found ({int(dt.seconds/60)}m {dt.seconds%60}s)")
+
+
+    cols = ["oidref", "tic", "kic", "epic", "gaia_dr3_id"]
+    aliases[cols] = aliases[cols].fillna(-1)
+
+    p = re.compile(r"\w+\s+(\d+)$")
+    def id_to_num(r: str):
+        row = []
+        for s in r:
+            if s is np.nan or s is None:
+                row.append(-1)
+                continue
+
+            if type(s) is int:
+                row.append(s)
+                continue
+            
+            m = p.search(s)
+            if m is None:
+                row.append(-1)
+            else:
+                row.append(int(m[1]))
+
+        return row
+
+    aliases[cols] = aliases[cols].apply(id_to_num, axis=1, result_type="expand")
 
     ##### SAVE TABLES
 
-    # check existence of directory for NEA tables
-    if not os.path.isdir("../db/nea/"):
-        os.mkdir("../db/nea/")
-
     # write tables to db/nea/ directory
-    ps.to_csv("../db/nea/ps.csv", index=False)
-    koi.to_csv("../db/nea/koi.csv", index=False)
-    k2.to_csv("../db/nea/k2.csv", index=False)
-    toi.to_csv("../db/nea/toi.csv", index=False)
-    al.to_csv("../db/nea/aliases.csv", index=False)
+    aliases.to_csv("../db/aliases.csv", index=False)
 
--- io/get_rotp.py
+++ io/get_rotp.py
@@ -3,158 +3,199 @@ import numpy as np
 import pandas as pd
 
 
-ps = pd.read_csv("../db/nea/ps.csv")
-k2 = pd.read_csv("../db/nea/k2.csv")
-koi = pd.read_csv("../db/nea/koi.csv")
-toi = pd.read_csv("../db/nea/toi.csv")
-
-ps_hosts = ps["hostname"]
-k2_hosts = k2["hostname"]
-koi_hosts = koi["kepid"].apply(lambda num: f"KIC {num:d}")
-toi_hosts = toi["tid"]
-
-hostnames = .
-
-
-
-
-
-
-def collect_by_tag(tag: str):
-    ...
-
-collect_by_tag("TIC")
+# load table of aliases for all exoplanet detections (candidates and confirmed)
+aliases = ascii.read("../db/aliases.csv").to_pandas()
 
 
 #------------------------------------------------------------------------------
-# NEA DATABASES
+# GAIA DR3 ID HOSTS
 #------------------------------------------------------------------------------
 
-# NEA PSCP
-nea_ps = pd.read_csv("../db/nea/ps.csv")
+# Lu+ 22
+lu22 = ascii.read("../db/gaia/lu22.mrt").to_pandas()
+lu22 = lu22[["EDR3", "Prot"]]
+lu22 = lu22.rename({"EDR3": "gaia_dr3_id"}, axis=1)
+lu22["db"] = "lu22"
 
-# PSCP aliases (KOI, KIC, EPIC, TOI, TIC)
-aliases = pd.read_csv("../db/nea/aliases.csv")
+# Christy+ 23 -- G Band data
+chr23g = ascii.read("../db/gaia/christy23_gband.csv", format="csv").to_pandas()
+chr23g = chr23g.loc[chr23g["ML_classification"] == "ROT", ["EDR3_source_id", "Period"]]
+chr23g = chr23g.rename({
+    "EDR3_source_id": "gaia_dr3_id",
+    "Period": "Prot"
+    }, axis=1)
+chr23g["db"] = "chr23g"
 
-# NEA Kepler
-nea_koi = pd.read_csv("../db/nea/koi.csv")
-nea_koi = nea_koi.rename({"kepid": "KIC"}, axis=1)
-nea_koi["koi_eccen"] = nea_koi["koi_eccen"].fillna(0.0)
+# Christy+ 23 -- V Band data
+chr23v = ascii.read("../db/gaia/christy23_vband.csv", format="csv").to_pandas()
+chr23v = chr23v.loc[chr23v["variable_type"] == "ROT", ["edr3_source_id", "period"]]
+chr23v = chr23v.rename({
+    "edr3_source_id": "gaia_dr3_id",
+    "period": "Prot"
+    }, axis=1)
+chr23v["db"] = "chr23v"
 
-# NEA K2
-nea_k2 = pd.read_csv("../db/nea/k2.csv")
-
-# NEA TOI
-nea_toi = pd.read_csv("../db/nea/toi.csv")
-nea_toi = nea_toi.rename({"tid": "TIC", "toipfx": "TOI"}, axis=1)
-
-
-
-
-
-
-
-
-
-#------------------------------------------------------------------------------
-# ASAS-SN DATABASES
-#------------------------------------------------------------------------------
+# Claytor+ 25
+cla25 = ascii.read("../db/gaia/claytor25.csv", format="csv").to_pandas()
+cla25 = cla25[["gaia_designation", "period", "period_err"]]
+cla25 = cla25.rename({
+    "period": "Prot",
+    "period_err": "E_Prot"
+    }, axis=1)
+cla25["gaia_dr3_id"] = cla25["gaia_designation"].apply(lambda s: int(s[-19:]))
+cla25 = cla25.drop("gaia_designation", axis=1)
+cla25["db"] = "cla25"
 
 # Schochet+ 25
-asassn_s25 = pd.read_csv("../db/asas-sn/schochet25.csv")
-asassn_s25 = asassn_s25[["dr3_source_id", "dr2_source_id", "KIC", "tic_id", "period", "sigma"]]
-asassn_s25 = asassn_s25.rename({"period": "Prot",
-                                "sigma": "E_Prot",
-                                "tic_id": "TIC",
-                                "dr2_source_id": "gaia_dr2_id",
-                                "dr3_source_id": "gaia_dr3_id"}, axis=1)
-asassn_s25["db"] = "s25"
+sch25 = ascii.read("../db/gaia/schochet25.csv", format="csv").to_pandas()
+sch25 = sch25[["dr3_source_id", "period", "sigma"]]
+sch25 = sch25.rename({
+    "dr3_source_id": "gaia_dr3_id",
+    "period": "Prot",
+    "sigma": "E_Prot",
+    }, axis=1)
+sch25["db"] = "sch25"
+
+# concatenate all prot tables using gaia_dr3_id to identify hosts
+prot_gdr3 = pd.concat([lu22, chr23g, chr23v, cla25, sch25])
+prot_gdr3 = pd.merge(aliases, prot_gdr3, on="gaia_dr3_id", how="left")
+print("gaia_dr3_id: " + str(prot_gdr3.loc[prot_gdr3["Prot"].notnull(), "oidref"].nunique()))
 
 
 
 #------------------------------------------------------------------------------
-# KEPLER DATABASES
+# KIC HOSTS
 #------------------------------------------------------------------------------
+
+# McQuillan+ 13
+mcq13 = ascii.read("../db/kic/mcquillan13.mrt").to_pandas()
+mcq13 = mcq13[["KID", "Period", "Period_Err"]]
+mcq13 = mcq13.rename({
+    "KID": "kic", 
+    "Period": "Prot",
+    "Period_Err": "E_Prot"
+    }, axis=1)
+mcq13["db"] = "mcq13"
+
+# McQuillan+ 14
+mcq14 = ascii.read("../db/kic/mcquillan14.mrt").to_pandas()
+mcq14 = mcq14[["KIC", "PRot", "e_PRot"]]
+mcq14 = mcq14.rename({
+    "KIC": "kic",
+    "PRot": "Prot",
+    "e_PRot": "E_Prot"
+    }, axis=1)
+mcq14["db"] = "mcq14"
 
 # Santos+ 19
-kep_s19 = ascii.read("../db/kepler/santos19.mrt").to_pandas()
-kep_s19 = kep_s19[["KIC", "Prot", "E_Prot"]]
-kep_s19["db"] = "s19"
+san19 = ascii.read("../db/kic/santos19.mrt").to_pandas()
+san19 = san19[["KIC", "Prot", "E_Prot"]]
+san19 = san19.rename({"KIC": "kic"}, axis=1)
+san19["db"] = "san19"
 
 # Santos+ 21
-kep_s21 = ascii.read("../db/kepler/santos21.mrt").to_pandas()
-kep_s21 = kep_s21[["KIC", "Prot", "E_Prot"]]
-kep_s21["db"] = "s21"
+san21 = ascii.read("../db/kic/santos21.mrt").to_pandas()
+san21 = san21[["KIC", "Prot", "E_Prot"]]
+san21 = san21.rename({"KIC": "kic"}, axis=1)
+san21["db"] = "san21"
 
 # Reinhold+ 23
-kep_r23 = ascii.read("../db/kepler/reinhold23.mrt", data_start=2).to_pandas()
-kep_r23 = kep_r23[["KIC", "ProtFin", "SNR"]]
-kep_r23 = kep_r23.rename({"ProtFin": "Prot"}, axis=1)
-kep_r23["E_Prot"] = kep_r23["Prot"] / kep_r23["SNR"]
-kep_r23["db"] = "r23"
-kep_r23 = kep_r23.drop(columns="SNR")
+rei23 = ascii.read("../db/kic/reinhold23.mrt").to_pandas()
+rei23 = rei23[["KIC", "ProtFin", "SNR"]]
+rei23 = rei23.rename({
+    "KIC": "kic",
+    "ProtFin": "Prot"
+    }, axis=1)
+rei23["E_Prot"] = rei23["Prot"] / rei23["SNR"]
+rei23["db"] = "rei23"
+rei23 = rei23.drop(columns="SNR")
+
+# concatenate all prot tables using kic to identify hosts
+prot_kic = pd.concat([mcq13, mcq14, san19, san21, rei23])
+prot_kic = pd.merge(aliases, prot_kic, on="kic", how="left")
+print("kic: " + str(prot_kic.loc[prot_kic["Prot"].notnull(), "oidref"].nunique()))
 
 
 
 #------------------------------------------------------------------------------
-# TESS DATABASES
+# EPIC HOSTS
 #------------------------------------------------------------------------------
+
+# Saylor+ 18
+say18 = ascii.read("../db/epic/saylor18.mrt").to_pandas()
+say18 = say18[["EPIC", "Prot", "e_Prot"]]
+say18 = say18.rename({
+    "EPIC": "epic",
+    "e_Prot": "E_Prot"
+    }, axis=1)
+say18["db"] = "say18"
+
+# concatenate all prot tables using epic to identify hosts
+prot_epic = say18
+prot_epic = pd.merge(aliases, prot_epic, on="epic", how="left")
+print("epic: " + str(prot_epic.loc[prot_epic["Prot"].notnull(), "oidref"].nunique()))
+
+
+
+#------------------------------------------------------------------------------
+# TIC HOSTS
+#------------------------------------------------------------------------------
+
+# Howard+ 20
+how20 = ascii.read("../db/tic/howard20.mrt").to_pandas()
+how20 = how20[["TIC", "Prot", "e_Prot"]]
+how20 = how20.rename({
+    "TIC": "tic",
+    "e_Prot": "E_Prot"
+    }, axis=1)
+how20["db"] = "how20"
+
+# Martins+ 20
+mar20 = ascii.read("../db/tic/martins20.mrt").to_pandas()
+mar20 = mar20[["TIC-ID", "Prot", "e_Prot"]]
+mar20 = mar20.rename({
+    "TIC-ID": "tic",
+    "e_Prot": "E_Prot"
+    }, axis=1)
+mar20["db"] = "mar20"
+
+# Holcomb+ 22
+hol22 = ascii.read("../db/tic/holcomb22.csv", format="csv").to_pandas()
+hol22 = hol22.loc[hol22["rotation_accepted"] == "True", ["TICID", "Prot"]]
+hol22 = hol22.rename({"TICID": "tic"}, axis=1)
+hol22["db"] = "hol22"
+
+# Claytor+ 24
+cla24 = ascii.read("../db/tic/claytor24.mrt").to_pandas()
+cla24 = cla24[["TIC", "prot", "e_prot"]]
+cla24 = cla24.rename({
+    "TIC": "tic",
+    "prot": "Prot",
+    "e_prot": "E_Prot"
+    }, axis=1)
+cla24["db"] = "cla24"
 
 # Colman+ 24
-table_c24 = pd.read_csv("../db/tess/colman24.csv")
-table_c24 = table_c24[["TIC", "Period", "Period sigma"]]
-table_c24 = table_c24.rename({"Period": "Prot", "Period sigma": "E_Prot"}, axis=1)
-table_c24["db"] = "c24"
+col24 = ascii.read("../db/tic/colman24.csv", format="csv").to_pandas()
+col24 = col24[["TIC", "Period", "Period sigma"]]
+col24 = col24.rename({
+    "TIC": "tic",
+    "Period": "Prot",
+    "Period sigma": "E_Prot"
+    }, axis=1)
+col24["db"] = "col24"
+
+# concatenate all prot tables using tic to identify hosts
+prot_tic = pd.concat([how20, mar20, hol22, cla24, col24])
+prot_tic = pd.merge(aliases, prot_tic, on="tic", how="left")
+print("tic: " + str(prot_tic.loc[prot_tic["Prot"].notnull(), "oidref"].nunique()))
 
 
+# MASTER PROT TABLE
 
-#------------------------------------------------------------------------------
-# MASTER DATABASES
-#------------------------------------------------------------------------------
+prot_master = pd.concat([prot_gdr3, prot_kic, prot_epic, prot_tic])
+print("all: " + str(prot_master.loc[prot_master["Prot"].notnull(), "oidref"].nunique()))
 
-# NEA confirmed planets
+print(prot_master.count())
+print(prot_master[prot_master["Prot"].notnull()].count())
 
-# GAIA DR3 from s25
-#ps_s25_gdr2 = pd.merge(nea_ps, asassn_s25[["Prot", "E_Prot", "gaia_dr2_id"]], on="gaia_dr2_id", how="inner")
-#ps_s25_gdr3 = pd.merge(nea_ps, asassn_s25[["Prot", "E_Prot", "gaia_dr3_id"]], on="gaia_dr3_id", how="inner")
-#ps_s25_kic = pd.merge(nea_ps, asassn_s25[["Prot", "E_Prot", "KIC"]], on="KIC", how="inner")
-#ps_s25_tic = pd.merge(nea_ps, asassn_s25[["Prot", "E_Prot", "TIC"]], on="TIC", how="inner")
-
-#ps_s25 = pd.concat([ps_s25_gdr2, ps_s25_gdr3, ps_s25_kic, ps_s25_tic])
-#ps_s25 = pd.merge(nea_ps, asassn_s25, on=["gaia_dr2_id", "gaia_dr3_id", "KIC", "TIC"], how="inner")
-ps_s25 = pd.merge(nea_ps, asassn_s25, on=["gaia_dr2_id"], how="inner")
-ps_s25.to_csv("../db/master/ps_s25.csv")
-
-pd.set_option("display.max_rows", 200)
-print(ps_s25.sort_values(by="pl_name"))
-
-
-"""
-
-# Kepler planets and candidates
-table_s19_koi = pd.merge(kep_s19, nea_koi, on="KIC", how="inner")
-table_s21_koi = pd.merge(kep_s21, nea_koi, on="KIC", how="inner")
-table_r23_koi = pd.merge(kep_r23, nea_koi, on="KIC", how="inner")
-asassn_s25_koi = pd.merge(asassn_s25, nea_koi, on="KIC", how="inner")
-
-table_koi = pd.concat([table_s19_koi,
-                       table_s21_koi,
-                       table_r23_koi,
-                       asassn_s25_koi],
-                      ignore_index=True)
-
-table_koi.to_csv("../db/master/koi.csv")
-
-# K2 planets and candidates
-
-
-# TESS
-table_c24_toi = pd.merge(table_c24, table_nea_toi, on="TIC", how="inner")
-asassn_s25_toi = pd.merge(asassn_s25, table_nea_toi, on="TIC", how="inner")
-
-table_toi = pd.concat([table_c24_toi,
-                       asassn_s25_toi],
-                      ignore_index=True)
-
-table_toi.to_csv("../db/master/toi.csv")
-"""
